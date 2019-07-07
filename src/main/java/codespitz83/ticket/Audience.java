2be2131317dea4cc4df67a72f5655a8431ecdb1c
Antop <antop@naver.com>
2019-07-07 12:24:07 +0900

    codespitz83-objects-1 01. 객체, 설계

---
+++ src/main/java/codespitz83/ticket/Audience.java
@@ -0,0 +1,19 @@
+package codespitz83.ticket;
+
+/**
+ * 관람객은 소지품을 보관하기 위해 가방을 소지할 수 있다.
+ */
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    // 가방의 존재를 숨겼다. (캡슐화)
+    // 가방이 아니라 작은 지갑을 소지하도록 코드를 변경하고 싶다면 Audience 내부면 변경하며 된다.
+    public Long buy(Ticket ticket) {
+        // 가방도 캡슐화
+        return bag.hold(ticket);
+    }
+}
---
+++ src/main/java/codespitz83/ticket/Ticket.java
@@ -0,0 +1,14 @@
+package codespitz83.ticket;
+
+/**
+ * 티켓
+ */
+public class Ticket {
+    // 요금
+    private Long fee;
+
+    public Long getFee() {
+        return fee;
+    }
+
+}
